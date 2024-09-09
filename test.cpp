779ea4ea74e412ba40aaefa2c0cbffc042eaf4b8
Justin Lawrence S. Pelovello <justinpelovello38@gmail.com>
2024-09-09 12:10:14 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+    using namespace std;
+        
+        int main()
+        {
+        	//local variable	
+            int num1=0,num2=0,sum=0;
+            int num3=0,num4=0,difference=0;
+            int num5=0,num6=0,product=0;
+            int num7=0,num8=0,quotient=0;
+            int num9=0,num10=0,mod=0;
+            
+            cout<<"Input for sum number 1: ";
+			cin>>num1;
+			cout<<"Input for sum number 2: ";
+			cin>>num2;
+			
+			sum = (num1+num2);
+				cout<<"The sum of two numbers is: "<<sum;
+			
+			cout<<"Input for Substraction number 1: ";
+			cin>>num3;
+			cout<<"Input for Substraction number 2: ";
+			cin>>num4;
+			
+			difference = (num4-num5);
+				cout<<"The difference of two numbers is: "<<difference;
+			
+			cout<<"Input for Multiplication number 1: ";
+			cin>>num5;
+			cout<<"Input for Multiplication number 2: ";
+			cin>>num6;
+			
+			product = (num5*num6);
+			    cout<<"The difference of two product numbers is: "<<product;
+			    
+			cout<<"Input for Division number 1: ";
+			cin>>num7;
+			cout<<"Input for Division number 2: ";
+			cin>>num8;
+			
+			quotient = (num7%num8);
+			    cout<<"The difference of two quotient numbers is: "<<quotient;
+			
+			cout<<"Input for Remainder number 1: ";
+			cin>>num9;
+			cout<<"Input for Remainder number 2: ";
+			cin>>num10;
+				
+			mod = (num9/num10);
+			    cout<<"The difference of two mod numbers is: "<<mod;
+					
+				    
+	
+	
+		}
+         
+		
