68618a30f1bbcdf47627d2cd81a5215aa944bed8
Justin Lawrence S. Pelovello <justinpelovello38@gmail.com>
2024-09-16 12:24:39 +0800

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+     using namespace std;
+    	int main()
+    {
+    		int i=0;
+    		int j=10;
+    		
+    	    for(i=0;i<=j;i++)
+        {
+	       	cout<<"Hello World: "<<i<<endl;
+		}	
+		return 0;
+	}
+	
