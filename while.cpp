35edfc5c9a315a5e1bdb6412d21827fac6b09f96
Justin Lawrence S. Pelovello <justinpelovello38@gmail.com>
2024-09-16 12:26:05 +0800

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+     using namespace std;
+    	int main()
+		
+	{
+			int i=0;
+				
+				while(i<=10)
+			{
+				cout<<"Hello World: "<<i<<endl;
+				i=i+1;
+			}
+		return 0;
+	}
+	 
+	 
+	 
+
