4c77b0242c343d65b71119eb514fe53ab7a38294
Justin Lawrence S. Pelovello <justinpelovello38@gmail.com>
2024-09-16 12:23:45 +0800

    Add files via upload

---
+++ Exercise 4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+     using namespace std;
+    	int main()
+		
+	{
+			int i=4;
+				
+				while(i<=9)
+			{
+				cout<<"Hello World: "<<i<<endl;
+				i=i+1;
+			}
+		return 0;
+	}
